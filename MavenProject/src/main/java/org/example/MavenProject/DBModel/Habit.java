1ce5c126d229a5494b9e8f5e9ca4ed67c98b5bd4
baileySmith1 <bayryansmith@gmail.com>
2025-03-31 15:45:13 -0400

    Added gary's habit model

---
+++ MavenProject/src/main/java/org/example/MavenProject/DBModel/Habit.java
@@ -0,0 +1,14 @@
+package org.example.MavenProject;
+
+public class Habit {
+
+    String name;
+    String description;
+    Boolean isCompleted = false;
+
+    public Habit(String name, String description){
+        this.name = name;
+        this.description = description;
+
+    }
+}
