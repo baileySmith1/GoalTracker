e9c3877688d380e98095b5bff84cfe0c04300ca5
GaryLore <garyperez1369@gmail.com>
2025-04-01 14:14:58 -0400

    Delete MavenProject/src/main/java/org/example/MavenProject/DBRepository/UserRepo.java
    Bailey said so

--- MavenProject/src/main/java/org/example/MavenProject/DBRepository/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package org.example.MavenProject.DBRepository;
-
-import org.example.MavenProject.DBModel.Users;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepo extends MongoRepository<Users, Integer> {
-
-}
